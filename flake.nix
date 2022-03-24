3362f74a08b07b304bd94101614ac290da2272b3
Pablo Ovelleiro Corral <mail@pablo.tools>
2022-03-24 17:30:31 +0100

    Add sops-nix

--- flake.nix
+++ flake.nix
@@ -1,6 +1,17 @@
 {
   description = "exec runner server";
-  inputs = { nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable"; };
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    pinpox.url = "github:pinpox/nixos";
+    pinpox.inputs.nixpkgs.follows = "nixpkgs";
+
+  sops-nix.url = github:Mic92/sops-nix;
+  sops-nix.inputs.nixpkgs.follows = "nixpkgs";
+
+    # mayniklas.url = "github:mayniklas/nixos";
+    # mayniklas.inputs.nixpkgs.follows = "nixpkgs";
+
+  };
   outputs = { self, ... }@inputs:
     with inputs;
     let
@@ -13,6 +24,16 @@
       lib = nixpkgs.lib;
     in {
       nixosConfigurations = {
+        minio-host = nixpkgs.lib.nixosSystem {
+          # system = "test${(builtins.readFile ./machines/minio-host/arch)}";
+          # system = "aarch64-linux";
+          system = import ./machines/minio-host/arch.nix;
+          modules = builtins.attrValues self.nixosModules ++ [
+            pinpox.nixosModules.openssh
+            sops-nix.nixosModules.sops
+            (import ./machines/minio-host/configuration.nix { inherit self; })
+          ];
+        };
 
         oracle-aarch64-runner-1 = nixpkgs.lib.nixosSystem {
           system = "aarch64-linux";
---
+++ machines/minio-host/configuration.nix
@@ -0,0 +1,38 @@
+{ self, ... }:
+{ pkgs, ... }: {
+
+  pinpox.services.openssh.enable = true;
+
+  lounge-rocks = {
+    oracle-aarch64.enable = true;
+    nix-build-signature.enable = true;
+    nix-common.enable = true;
+  };
+
+  networking = { hostName = "minio-host"; };
+
+  networking.firewall.allowedTCPPorts = [ 9000 9001 ];
+
+  services.minio = {
+    enable = true;
+    listenAddress = "127.0.0.1:9000";
+    consoleAddress = "127.0.0.1:9001";
+    region = "eu-central-1";
+
+    rootCredentialsFile = "/var/src/secrets/minio/env";
+# MINIO_ROOT_USER=admin
+# MINIO_ROOT_PASSWORD=XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+
+    # dataDir = [ "/mnt/data/minio/data" ];
+    # configDir = "/mnt/data/minio/config";
+  };
+
+  systemd.services.minio = {
+
+    environment = {
+      MINIO_SERVER_URL = "https://s3.pablo.tools";
+      MINIO_BROWSER_REDIRECT_URL = "https://minio.lounge.rocks";
+    };
+  };
+
+}
