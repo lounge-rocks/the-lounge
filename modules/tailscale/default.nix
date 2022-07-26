c9666a65bc2a0e739b3f62c52e861ed36dea49dc
MayNiklas <info@niklas-steffen.de>
2022-07-26 23:28:51 +0200

    add tailscale module

---
+++ modules/tailscale/default.nix
@@ -0,0 +1,26 @@
+{ config, pkgs, lib, ... }:
+with lib;
+let cfg = config.lounge-rocks.tailscale;
+in
+{
+
+  options.lounge-rocks.tailscale = {
+    enable = mkEnableOption "tailscale";
+    exitNode = mkEnableOption "tailscale exit node";
+  };
+
+  config = mkIf cfg.enable {
+
+    services.tailscale = {
+      enable = true;
+      interfaceName = "tailscale0";
+      package = pkgs.tailscale;
+      port = 39140;
+    };
+
+    # strict reverse path filtering breaks Tailscale exit node use and some subnet routing setups
+    networking.firewall.checkReversePath = mkIf cfg.exitNode "loose";
+
+  };
+
+}
