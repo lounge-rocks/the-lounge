cad2a33acf0b17eadd5c2a0b2ba6a538960717e9
Pablo Ovelleiro Corral <mail@pablo.tools>
2023-02-04 14:53:48 +0100

    Woodpecker ci (#3)
    * Add initial files
    
    * add modules
    
    * Hardcode user home
    
    * Try to fix missing uname
    
    * Add coreutils
    
    * Update flake.lock
    
    * Remove accidentally commited file

--- machines/woodpecker-server/configuration.nix
+++ machines/woodpecker-server/configuration.nix
@@ -1,5 +1,70 @@
 { self, ... }:
-{ pkgs, ... }: {
+{ pkgs, pinpox-woodpecker, config, ... }: {
+
+
+  nix.settings.allowed-users = [ config.services.woodpecker-agent.user "woodpecker-agent" ];
+
+  sops.defaultSopsFile = ../../secrets/woodpecker-server/secrets.yaml;
+  sops.secrets = {
+    # "woodpecker/gitea-client-id".restartUnits = [ "woodpecker-server.service" ];
+    # "woodpecker/gitea-client-secret".restartUnits = [ "woodpecker-server.service" ];
+    "woodpecker/server-envfile".restartUnits = [ "woodpecker-server.service" ];
+
+    "woodpecker/agent-secret".restartUnits = [ "woodpecker-agent.service" "woodpecker-server.service" ];
+  };
+  # rootCredentialsFile = config.sops.secrets."minio/env".path;
+
+  networking.firewall.allowedTCPPorts = [ 443 80 ];
+
+  services.woodpecker-server = {
+    # giteaUrl = "https://git.0cx.de";
+
+    useGitea = false;
+
+    package = pinpox-woodpecker.packages.x86_64-linux.woodpecker-server;
+    enable = true;
+    rootUrl = "https://build.lounge.rocks";
+    httpPort = 3030;
+    admins = "pinpox";
+    database = {
+      type = "sqlite3";
+    };
+    # giteaClientIdFile = "${config.sops.screts."woodpecker/gitea-client-id".path}";
+    # giteaClientSecretFile = "${config.sops.secrets."woodpecker/gitea-client-secret".path}";
+    agentSecretFile = "${config.sops.secrets."woodpecker/agent-secret".path}";
+    environmentFile = "${config.sops.secrets."woodpecker/server-envfile".path}";
+  };
+
+  services.woodpecker-agent = {
+    enable = true;
+    backend = "local";
+    maxProcesses = 5;
+    agentSecretFile = "${config.sops.secrets."woodpecker/agent-secret".path}";
+
+    package = pinpox-woodpecker.packages.x86_64-linux.woodpecker-agent;
+  };
+
+  security.acme.acceptTerms = true;
+  security.acme.defaults.email = "acme@pablo.tools";
+
+  services.nginx = {
+    enable = true;
+    recommendedOptimisation = true;
+    recommendedTlsSettings = true;
+    clientMaxBodySize = "128m";
+
+    virtualHosts = {
+
+      "build.lounge.rocks" = {
+        forceSSL = true;
+        enableACME = true;
+        locations."/" = { proxyPass = "http://127.0.0.1:3030"; };
+      };
+
+    };
+  };
+
+  # General stuff
 
   lounge-rocks = {
     hetzner-x86.enable = true;
@@ -26,3 +91,5 @@
   system.stateVersion = "22.05";
 
 }
+
+
