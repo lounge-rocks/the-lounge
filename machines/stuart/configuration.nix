1ba1c11a9ef2b326b6bd75a7cd5ab911ba30e194
Pablo Ovelleiro Corral <mail@pablo.tools>
2023-07-18 16:06:43 +0200

    proxy websockets in nginx for s3

--- machines/stuart/configuration.nix
+++ machines/stuart/configuration.nix
@@ -89,6 +89,8 @@
         locations = {
           "/" = {
             proxyPass = "http://127.0.0.1:9001";
+            proxyWebsockets = true;
+
             extraConfig = ''
               proxy_set_header X-Real-IP $remote_addr;
               proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
@@ -129,6 +131,7 @@
         locations = {
           "/" = {
             proxyPass = "http://127.0.0.1:9000";
+            proxyWebsockets = true;
             extraConfig = ''
               proxy_set_header X-Real-IP $remote_addr;
               proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
