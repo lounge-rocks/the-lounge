91c80f99a4aae994f6fd11374065dcc27702918e
Pablo Ovelleiro Corral <mail@pablo.tools>
2023-07-18 16:31:36 +0200

    S3 websocket fix (#5)
    * proxy websockets in nginx for s3
    
    * Update flake.lock
    
    * fix nginx config on stuart

--- machines/stuart/configuration.nix
+++ machines/stuart/configuration.nix
@@ -97,9 +97,6 @@
               proxy_set_header X-Forwarded-Proto $scheme;
               # proxy_set_header Host $host;
               proxy_connect_timeout 300;
-              # Default is HTTP/1, keepalive is only enabled in HTTP/1.1
-              proxy_http_version 1.1;
-              proxy_set_header Connection "";
               chunked_transfer_encoding off;
             '';
           };
@@ -131,7 +128,6 @@
         locations = {
           "/" = {
             proxyPass = "http://127.0.0.1:9000";
-            proxyWebsockets = true;
             extraConfig = ''
               proxy_set_header X-Real-IP $remote_addr;
               proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
