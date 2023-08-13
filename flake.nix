99e1a1ea74460714f512f5d55c5eefcd9ca091b1
MayNiklas <info@niklas-steffen.de>
2023-08-13 11:39:47 +0200

    cachix: 1.3.1 -> 1.6

--- flake.nix
+++ flake.nix
@@ -32,7 +32,7 @@
     mayniklas.inputs.nixpkgs.follows = "nixpkgs";
     mayniklas.inputs.flake-utils.follows = "flake-utils";
 
-    cachix.url = "github:cachix/cachix/v1.3.1";
+    cachix.url = "github:cachix/cachix/v1.6";
 
   };
   outputs = { self, ... }@inputs:
