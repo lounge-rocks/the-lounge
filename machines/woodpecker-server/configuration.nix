bb8a16b1e71ef583c3b3722d68a5ac5109fc0fe3
MayNiklas <info@niklas-steffen.de>
2023-08-15 17:53:33 +0200

    add woodpecker docker agent

--- machines/woodpecker-server/configuration.nix
+++ machines/woodpecker-server/configuration.nix
@@ -73,6 +73,7 @@
       ipv6_address = "2a01:4f8:1c17:636f::";
     };
     nix-common.enable = true;
+    woodpecker.docker-agent.enable = true;
     woodpecker.local-agent.enable = true;
     woodpecker.pipeliner.enable = true;
     woodpecker.server.enable = true;
---
+++ modules/woodpecker/docker-agent.nix
@@ -0,0 +1,43 @@
+{ lib, config, ... }:
+with lib;
+let cfg = config.lounge-rocks.woodpecker.docker-agent; in
+{
+
+  options.lounge-rocks.woodpecker.docker-agent = {
+    enable = mkEnableOption "enable docker woodpecker agent";
+  };
+
+  config = mkIf cfg.enable {
+
+    services.woodpecker-agents = {
+      agents.docker = {
+        enable = true;
+        environment = {
+          WOODPECKER_SERVER = "127.0.0.1:9000";
+          WOODPECKER_MAX_WORKFLOWS = "10";
+          WOODPECKER_BACKEND = "docker";
+          WOODPECKER_FILTER_LABELS = "type=docker";
+          WOODPECKER_HEALTHCHECK = "false";
+        };
+        # Secrets in envfile: WOODPECKER_AGENT_SECRET
+        environmentFile = [ config.sops.secrets."woodpecker/agent-envfile".path ];
+        extraGroups = [ "docker" ];
+      };
+    };
+
+    virtualisation.docker = {
+      enable = true;
+      autoPrune = {
+        enable = true;
+        dates = "weekly";
+      };
+    };
+
+    systemd.services.woodpecker-agent-docker = {
+      after = [ "docker.socket" ];
+      restartIfChanged = false;
+      serviceConfig = { BindPaths = [ "/var/run/docker.sock" ]; };
+    };
+
+  };
+}
