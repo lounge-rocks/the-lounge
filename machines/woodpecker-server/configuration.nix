1e3679e4bc11c42376e432260d9607898b1b3cd5
MayNiklas <info@niklas-steffen.de>
2023-08-17 10:46:34 +0200

    enable attic

--- machines/woodpecker-server/configuration.nix
+++ machines/woodpecker-server/configuration.nix
@@ -23,7 +23,7 @@
       ipv6_address = "2a01:4f8:1c17:636f::";
     };
     nix-common.enable = true;
-    # attic.server.enable = true;
+    attic.server.enable = true;
     woodpecker.docker-agent.enable = true;
     woodpecker.local-agent.enable = true;
     woodpecker.pipeliner.enable = true;
