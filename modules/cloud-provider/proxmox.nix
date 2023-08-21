9e59eebb4e1be17610521f595722a8194e13dfbc
MayNiklas <info@niklas-steffen.de>
2023-08-21 18:44:53 +0200

    make disko config easier to read

--- modules/cloud-provider/proxmox.nix
+++ modules/cloud-provider/proxmox.nix
@@ -18,47 +18,41 @@ in {
   config = mkIf cfg.enable {
 
     ### Partitioning ###
-    disko = {
-      devices = {
-        disk = {
-          main = {
-            type = "disk";
-            device = "/dev/vda";
+    disko.devices.disk.main = {
+      type = "disk";
+      device = "/dev/vda";
+      content = {
+        type = "table";
+        format = "gpt";
+        partitions = [
+          {
+            name = "boot";
+            start = "0";
+            end = "1M";
+            flags = [ "bios_grub" ];
+          }
+          {
+            name = "ESP";
+            start = "1M";
+            end = "512M";
+            bootable = true;
             content = {
-              type = "table";
-              format = "gpt";
-              partitions = [
-                {
-                  name = "boot";
-                  start = "0";
-                  end = "1M";
-                  flags = [ "bios_grub" ];
-                }
-                {
-                  name = "ESP";
-                  start = "1M";
-                  end = "512M";
-                  bootable = true;
-                  content = {
-                    type = "filesystem";
-                    format = "vfat";
-                    mountpoint = "/boot";
-                  };
-                }
-                {
-                  name = "nixos";
-                  start = "512M";
-                  end = "100%";
-                  content = {
-                    type = "filesystem";
-                    format = "ext4";
-                    mountpoint = "/";
-                  };
-                }
-              ];
+              type = "filesystem";
+              format = "vfat";
+              mountpoint = "/boot";
             };
-          };
-        };
+          }
+          {
+            name = "nixos";
+            start = "512M";
+            end = "100%";
+            content = {
+              type = "filesystem";
+              format = "ext4";
+              mountpoint = "/";
+            };
+          }
+        ];
       };
     };
 
