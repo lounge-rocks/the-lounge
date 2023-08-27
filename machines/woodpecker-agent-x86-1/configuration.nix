21f786665ada01cf7910e78dbeafd9d5cc67d841
MayNiklas <info@niklas-steffen.de>
2023-08-27 20:08:46 +0200

    enable woodpecker

--- machines/woodpecker-agent-x86-1/configuration.nix
+++ machines/woodpecker-agent-x86-1/configuration.nix
@@ -7,8 +7,13 @@
     nix-common.enable = true;
     tailscale.enable = true;
     users.MayNiklas.root = true;
+    woodpecker = {
+      docker-agent.enable = true;
+      local-agent.enable = true;
+    };
   };
 
+  boot.binfmt.emulatedSystems = [ "aarch64-linux" ];
   networking.hostName = "woodpecker-agent-x86-1";
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
