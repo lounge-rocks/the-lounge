fb816148e309fd7e73edc16b0a75f679d184f5ab
MayNiklas <info@niklas-steffen.de>
2023-11-11 14:12:49 +0100

    CI now also builds x86

--- pkgs/woodpecker-pipeline/default.nix
+++ pkgs/woodpecker-pipeline/default.nix
@@ -1,5 +1,6 @@
-# nix build .#woodpecker-pipeline && cat result| jq '.configs[].data' -r | jq > .woodpecker.yaml
-{ pkgs, flake-self, inputs }:
+# nix build .#woodpecker-pipeline && cat result| jq '.configs[].data' -r | jq > .woodpecker/x86-linux.yaml
+# nix build .#woodpecker-pipeline && cat result| jq '.configs[].data' -r | jq > .woodpecker/arm64-linux.yaml
+{ pkgs, lib, flake-self, inputs }:
 with pkgs;
 writeText "pipeline" (builtins.toJSON {
   configs =
@@ -8,6 +9,11 @@ writeText "pipeline" (builtins.toJSON {
       woodpecker-platforms = {
         "aarch64-linux" = "linux/arm64";
         "x86_64-linux" = "linux/amd64";
+      };
+      nixFlakeShow = {
+        name = "Nix flake show";
+        image = "bash";
+        commands = [ "nix flake show" ];
       };
       atticSetupStep = {
         name = "Setup Attic";
@@ -18,7 +24,6 @@ writeText "pipeline" (builtins.toJSON {
         secrets = [ "attic_key" ];
       };
     in
-    # Hosts
     pkgs.lib.lists.flatten ([
       (map
         (arch: {
@@ -27,24 +32,40 @@ writeText "pipeline" (builtins.toJSON {
             labels.backend = "local";
             # platform will be deprecated in the future!
             platform = woodpecker-platforms."${arch}";
-            steps = pkgs.lib.lists.flatten ([ atticSetupStep ] ++ (map
+            steps = pkgs.lib.lists.flatten ([ nixFlakeShow ] ++ [ atticSetupStep ]
+              ++ (map
               (host:
                 # only build hosts for the arch we are currently building
-                if
-                  (flake-self.nixosConfigurations.${host}.pkgs.stdenv.hostPlatform.system != arch) then
+                if (flake-self.nixosConfigurations.${host}.pkgs.stdenv.hostPlatform.system
+                  != arch) then
                   [ ]
                 else [
                   {
-                    name = "Build configuration for ${host}";
+                    name = "Build ${host}";
                     image = "bash";
                     commands = [
                       "nix build '.#nixosConfigurations.${host}.config.system.build.toplevel' -o 'result-${host}'"
-                      "attic push lounge-rocks:nix-cache 'result-${host}'"
                     ];
+                  }
+                  {
+                    "name" = "Show ${host} info";
+                    "image" = "bash";
+                    "commands" = [
+                      "nix path-info --closure-size -h $(readlink -f 'result-${host}')"
+                    ];
+                  }
+                  {
+                    name = "Push ${host} to Attic";
+                    image = "bash";
+                    commands =
+                      [ "attic push lounge-rocks:nix-cache 'result-${host}'" ];
                   }
                 ])
               (builtins.attrNames flake-self.nixosConfigurations)));
           });
-        }) [ "x86_64-linux" "aarch64-linux" ])
+        }) [
+        "x86_64-linux"
+        "aarch64-linux"
+      ])
     ]);
 })
