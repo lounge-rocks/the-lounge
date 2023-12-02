1bd784aa595af90456f9a65573ca21d94694ca97
MayNiklas <info@niklas-steffen.de>
2023-12-02 13:30:07 +0100

    create jitsi module

---
+++ modules/jitsi/default.nix
@@ -0,0 +1,50 @@
+{ lib, pkgs, config, ... }:
+with lib;
+let cfg = config.lounge-rocks.jitsi;
+in
+{
+
+  options.lounge-rocks.jitsi = {
+    enable = mkEnableOption "jitsi";
+    hostname = mkOption {
+      type = types.str;
+      default = "meet.lounge.rocks";
+    };
+  };
+
+  config = mkIf cfg.enable {
+
+    services = {
+      jitsi-videobridge.openFirewall = true;
+      jitsi-meet = {
+        enable = true;
+        hostName = "${cfg.hostname}";
+        config = {
+          defaultLang = "en";
+          enableWelcomePage = true;
+          requireDisplayName = true;
+          analytics.disabled = true;
+          startAudioOnly = true;
+          channelLastN = 4;
+          stunServers = [
+            { urls = "turn:turn.matrix.org:3478?transport=udp"; }
+            { urls = "turn:turn.matrix.org:3478?transport=tcp"; }
+          ];
+          constraints.video.height = {
+            ideal = 720;
+            max = 1080;
+            min = 240;
+          };
+        };
+        interfaceConfig = {
+          SHOW_JITSI_WATERMARK = false;
+          SHOW_WATERMARK_FOR_GUESTS = false;
+          DISABLE_PRESENCE_STATUS = true;
+          GENERATE_ROOMNAMES_ON_WELCOME_PAGE = false;
+        };
+      };
+    };
+
+  };
+
+}
