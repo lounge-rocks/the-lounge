2dc7d9eee080b14469ea15215cba0f8e970a585f
MayNiklas <info@niklas-steffen.de>
2024-01-17 16:02:31 +0100

    woodpecker -> 2.1.1

--- pkgs/woodpecker/common.nix
+++ pkgs/woodpecker/common.nix
@@ -1,9 +1,9 @@
 { lib, fetchFromGitHub }:
 let
-  version = "1.0.5";
-  srcHash = "sha256-tkgkhYuLHfmT42P+UMZ7uNB2wBKo0YGiw0a5RoMAu6M=";
-  vendorHash = "sha256-QTTnTPOgP+FlbqXNCGJc9VuBzQcMujpvFB3+DykYTPY=";
-  yarnHash = "sha256-TrcTc5svLLSedRC8gCwIBW7/mtHo+uSNZGImtRiVJ0w=";
+  version = "2.1.1";
+  srcHash = "sha256-rDnfJ7tNf5fBIpP69R2uLCfPC+zYO/CKJL6fsdX9pIQ=";
+  vendorHash = "sha256-hFf2vjefum+pdA7BR+4nFmqrPKOkPqij2KFLX+ew+4U=";
+  yarnHash = "sha256-XouDPWizy5TPfttyq93AqIsz9j6NRL4B1z8xNfI1IKs=";
 in
 {
   inherit version yarnHash vendorHash;
@@ -26,7 +26,7 @@ in
   ldflags = [
     "-s"
     "-w"
-    "-X github.com/woodpecker-ci/woodpecker/version.Version=${version}"
+    "-X go.woodpecker-ci.org/woodpecker/version.Version=${version}"
   ];
 
   meta = with lib; {
