aa7975b2233b983684c6dcf3eb13f3c8fb78e010
MayNiklas <info@niklas-steffen.de>
2024-08-31 15:13:25 +0200

    add nix-community.cachix.org

--- modules/nix-common/default.nix
+++ modules/nix-common/default.nix
@@ -83,11 +83,13 @@ in {
         substituters = mkIf (cfg.disable-cache != true) [
           "https://cache.lounge.rocks/nix-cache"
           "https://cuda-maintainers.cachix.org"
+          "https://nix-community.cachix.org"
           "https://numtide.cachix.org"
         ];
         trusted-public-keys = mkIf (cfg.disable-cache != true) [
           "nix-cache:4FILs79Adxn/798F8qk2PC1U8HaTlaPqptwNJrXNA1g="
           "cuda-maintainers.cachix.org-1:0dq3bujKpuEPMCX6U4WylrUDZ9JyUG0VpVZa7CNfq5E="
+          "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
           "numtide.cachix.org-1:2ps1kLBUWjxIneOy1Ik6cQjb41X0iXVXeHigGmycPPE="
         ];
         # Save space by hardlinking store files
