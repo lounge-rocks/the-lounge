ee84b6a314c2b83fd58222e4860b70c31878e8b0
MayNiklas <info@niklas-steffen.de>
2025-11-28 15:08:31 +0100

    do not import pipeliner.nix

--- modules/woodpecker/default.nix
+++ modules/woodpecker/default.nix
@@ -4,26 +4,15 @@ with lib;
 
 let
 
-  # Recursively constructs an attrset of a given folder, recursing on directories, value of attrs is the filetype
-  getDir = dir:
-    mapAttrs
-      (file: type: if type == "directory" then getDir "${dir}/${file}" else type)
-      (builtins.readDir dir);
-
-  # Collects all files of a directory as a list of strings of paths
-  files = dir:
-    collect isString
-      (mapAttrsRecursive (path: type: concatStringsSep "/" path) (getDir dir));
-
-  # Filters out directories that don't end with .nix or are this file, also makes the strings absolute
-  validFiles = dir:
-    map (file: ./. + "/${file}") (filter (file: hasSuffix ".nix" file && file != "default.nix") (files dir));
-
   cfg = config.lounge-rocks.woodpecker;
 
 in
 {
-  imports = validFiles ./.;
+  imports = [
+    ./docker-agent.nix
+    ./local-agent.nix
+    ./server.nix
+  ];
 
   # options affecting the woodpecker server as well as the woodpecker agents
   options.lounge-rocks.woodpecker = {
@@ -36,26 +25,27 @@ in
 
   config = {
 
-    services.woodpecker-server =
-      lib.mkIf config.lounge-rocks.woodpecker.server.enable {
-        environment = {
-          WOODPECKER_LOG_LEVEL = "${cfg.log}";
-        };
+    services.woodpecker-server = lib.mkIf config.lounge-rocks.woodpecker.server.enable {
+      environment = {
+        WOODPECKER_LOG_LEVEL = "${cfg.log}";
       };
+    };
 
     services.woodpecker-agents.agents.exec =
-      lib.mkIf config.lounge-rocks.woodpecker.local-agent.enable {
-        environment = {
-          WOODPECKER_LOG_LEVEL = "${cfg.log}";
+      lib.mkIf config.lounge-rocks.woodpecker.local-agent.enable
+        {
+          environment = {
+            WOODPECKER_LOG_LEVEL = "${cfg.log}";
+          };
         };
-      };
 
     services.woodpecker-agents.agents.docker =
-      lib.mkIf config.lounge-rocks.woodpecker.docker-agent.enable {
-        environment = {
-          WOODPECKER_LOG_LEVEL = "${cfg.log}";
+      lib.mkIf config.lounge-rocks.woodpecker.docker-agent.enable
+        {
+          environment = {
+            WOODPECKER_LOG_LEVEL = "${cfg.log}";
+          };
         };
-      };
 
   };
 
