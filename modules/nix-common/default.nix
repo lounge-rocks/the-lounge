6bc16a7408135be72042bff7ad96c9c4c43b1b8a
MayNiklas <info@niklas-steffen.de>
2026-06-03 23:58:01 +0200

    add cache.nixos-cuda.org

--- modules/nix-common/default.nix
+++ modules/nix-common/default.nix
@@ -81,12 +81,14 @@ in {
       settings = {
         substituters = mkIf (cfg.disable-cache != true) [
           "https://cache.lounge.rocks/nix-cache"
+          "https://cache.nixos-cuda.org"
           "https://cuda-maintainers.cachix.org"
           "https://nix-community.cachix.org"
           "https://numtide.cachix.org"
         ];
         trusted-public-keys = mkIf (cfg.disable-cache != true) [
           "nix-cache:4FILs79Adxn/798F8qk2PC1U8HaTlaPqptwNJrXNA1g="
+          "cache.nixos-cuda.org:74DUi4Ye579gUqzH4ziL9IyiJBlDpMRn9MBN8oNan9M="
           "cuda-maintainers.cachix.org-1:0dq3bujKpuEPMCX6U4WylrUDZ9JyUG0VpVZa7CNfq5E="
           "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
           "numtide.cachix.org-1:2ps1kLBUWjxIneOy1Ik6cQjb41X0iXVXeHigGmycPPE="
